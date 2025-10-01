be262859f22ebb208328f62359e7083fa5b00792
AlexisC183 <cuartoangel1213@gmail.com>
2025-10-01 10:17:26 -0700

    Vista principal

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views import View
+
+# Crea tus vistas aqui.
+
+class PrincipalView(View):
+    """Vista que renderiza el único archivo index.html de la aplicación."""
+
+    def get(self, peticion):
+        """
+        Renderiza el index.html cuando se accede a la app vía URL, siempre y cuando la URL no coincida con la de otra vista.
+        """
+
+        return render(peticion, 'index.html')
