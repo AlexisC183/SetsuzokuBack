803c579a0505455ea0a2ab76cb1e7e81eddd1a24
AlexisC183 <cuartoangel1213@gmail.com>
2025-10-02 11:07:03 -0700

    Cambio 1

--- core/views.py
+++ core/views.py
@@ -7,6 +7,6 @@ class PrincipalView(View):
     """Vista que renderiza el único archivo index.html de la aplicación."""
 
     def get(self, peticion):
-        """Renderiza el index.html cuando se accede a la app vía URL, siempre y cuando la URL no coincida con la de otra vista."""
+        """Renderiza el <cambio1>.html cuando se accede a la app vía URL, siempre y cuando la URL no coincida con la de otra vista."""
 
         return render(peticion, 'index.html')
